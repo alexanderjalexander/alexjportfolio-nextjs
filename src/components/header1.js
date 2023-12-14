fe15684bd61aa6403fe202eca1e7541932ca1f99
alexanderjalexander <alexj18082002@gmail.com>
2023-12-14 13:45:24 -0500

    Removing all components

--- src/components/header1.js
+++
@@ -1,8 +0,0 @@
-import { hauser_italic } from "../fonts";
-import styles from './header1.module.css'
-
-export default function Header1({title}) {
-    <div className={hauser_italic.className}>
-        <h1 className={styles.header1}>{title}</h1>
-    </div>
-}
