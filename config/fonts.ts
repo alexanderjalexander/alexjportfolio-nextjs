42f966ac9ffa686e657ea6fc9822885811becd33
alexanderjalexander <alexj18082002@gmail.com>
2023-12-19 18:03:03 -0500

    Changing fonts

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,8 +1,9 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google"
+import { Fira_Code as FontMono, Fira_Sans as FiraSans } from "next/font/google"
 
-export const fontSans = FontSans({
+export const firaSans = FiraSans({
   subsets: ["latin"],
-  variable: "--font-sans",
+  weight: "400",
+  variable: "--fira-sans",
 })
 
 export const fontMono = FontMono({
