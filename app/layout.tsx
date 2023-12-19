98cb3d8ab9c4999a38e8cc2388e246e2c3c08bcc
alexanderjalexander <alexj18082002@gmail.com>
2023-12-19 18:06:26 -0500

    Deletions & layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
+import { firaSans } from "@/config/fonts";
 import { Providers } from "./providers";
 import { Navbar } from "@/components/navbar";
 import { Link } from "@nextui-org/link";
@@ -34,7 +34,7 @@ export default function RootLayout({children,}: {children: React.ReactNode;}) {
 			<body
 				className={clsx(
 					"min-h-screen bg-background font-sans antialiased",
-					fontSans.variable
+					firaSans.variable
 				)}
 			>
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
@@ -44,15 +44,7 @@ export default function RootLayout({children,}: {children: React.ReactNode;}) {
 							{children}
 						</main>
 						<footer className="w-full flex items-center justify-center py-3">
-							<Link
-								isExternal
-								className="flex items-center gap-1 text-current"
-								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-								title="nextui.org homepage"
-							>
-								<span className="text-default-600">Powered by</span>
-								<p className="text-primary">NextUI</p>
-							</Link>
+							
 						</footer>
 					</div>
 				</Providers>
