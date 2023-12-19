a11a4df28ac7f1f9ce4eb93d9b8560d327e83667
alexanderjalexander <alexj18082002@gmail.com>
2023-12-19 18:42:25 -0500

    Icon changes, new linkedin one

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -17,7 +17,7 @@ import clsx from "clsx";
 
 import { ThemeSwitch } from "@/components/theme-switch";
 import {
-	GithubIcon,
+	GithubIcon, LinkedInIcon,
 } from "@/components/icons";
 
 export const Navbar = () => {
@@ -29,7 +29,7 @@ export const Navbar = () => {
 						<p className="font-bold text-inherit">AJ</p>
 					</NextLink>
 				</NavbarBrand>
-				<ul className="hidden lg:flex gap-4 justify-start ml-2">
+				<ul className="hidden md:flex gap-4 justify-start ml-2">
 					{siteConfig.navItems.map((item) => (
 						<NavbarItem key={item.href}>
 							<NextLink
@@ -47,29 +47,22 @@ export const Navbar = () => {
 				</ul>
 			</NavbarContent>
 
-			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+			<NavbarContent className="basis-1 pl-4" justify="end">
 				<Link isExternal href={siteConfig.links.github} aria-label="Github">
 					<GithubIcon className="text-default-500" />
 				</Link>
+				<Link isExternal href={siteConfig.links.linkedin} aria-label="LinkedIn">
+					<LinkedInIcon className="text-default-500" />
+				</Link>
 				<ThemeSwitch />
-				<NavbarMenuToggle />
+				<NavbarMenuToggle className="md:hidden" />
 			</NavbarContent>
 
 			<NavbarMenu>
 				<div className="mx-4 mt-2 flex flex-col gap-2">
 					{siteConfig.navItems.map((item, index) => (
 						<NavbarMenuItem key={`${item}-${index}`}>
-							<Link
-								color={
-									index === 2
-										? "primary"
-										: index === siteConfig.navItems.length - 1
-										? "danger"
-										: "foreground"
-								}
-								href="#"
-								size="lg"
-							>
+							<Link href="#" color="foreground" size="lg">
 								{item.label}
 							</Link>
 						</NavbarMenuItem>
