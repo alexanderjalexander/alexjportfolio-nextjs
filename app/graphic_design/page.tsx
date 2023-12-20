3c991ad560c076e30777f993fc41f82bda4f4886
alexanderjalexander <alexj18082002@gmail.com>
2023-12-19 19:01:48 -0500

    Adding all necessary pages

---
+++ app/graphic_design/page.tsx
@@ -0,0 +1,7 @@
+export default function Graphic_Design() {
+	return (
+		<div>
+
+		</div>
+	);
+}
---
+++ app/motion_graphics/page.tsx
@@ -0,0 +1,7 @@
+export default function Motion_Graphics() {
+	return (
+		<div>
+
+		</div>
+	);
+}
