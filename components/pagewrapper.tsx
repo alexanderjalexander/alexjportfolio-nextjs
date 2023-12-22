f22ed30151fa591150f1296c74bcc94d39ff8786
alexanderjalexander <alexj18082002@gmail.com>
2023-12-22 18:24:21 -0500

    Hide Spinner

--- components/pagewrapper.tsx
+++ components/pagewrapper.tsx
@@ -8,13 +8,13 @@ export function PageWrapper({children, className,}:
 { children: React.ReactNode; className?: string;}) {
     return (
         <div>
-            <motion.div
+            {/* <motion.div
             initial={{ opacity: 1 }}
-            // transition={{ delay: 0.25 }}
+            transition={{ delay: 0.25 }}
             animate={{ opacity: 0 }}
                exit={{ opacity: 1 }}>
                 <Spinner className="fixed left-0 top-0 bottom-0 right-0" color={"white"} size="lg"/>
-            </motion.div>
+            </motion.div> */}
             <motion.div key={0}
             initial={{ opacity: 0, y: -100 }}
             // transition={{ delay: 0.25 }}
