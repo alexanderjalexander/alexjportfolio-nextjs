6349fa37c67420f2427b344ed1eb90b379b187e3
alexanderjalexander <alexj18082002@gmail.com>
2023-12-22 18:23:50 -0500

    PageWrapper changes

--- components/pagewrapper.tsx
+++ components/pagewrapper.tsx
@@ -1,9 +1,8 @@
 "use client"
 
-import classNames from "classnames";
 import { motion } from "framer-motion";
 import { Spinner } from "@nextui-org/react"
-import { setTimeout } from "timers";
+import classNames from "classnames";
 
 export function PageWrapper({children, className,}: 
 { children: React.ReactNode; className?: string;}) {
@@ -11,17 +10,19 @@ export function PageWrapper({children, className,}:
         <div>
             <motion.div
             initial={{ opacity: 1 }}
+            // transition={{ delay: 0.25 }}
             animate={{ opacity: 0 }}
                exit={{ opacity: 1 }}>
                 <Spinner className="fixed left-0 top-0 bottom-0 right-0" color={"white"} size="lg"/>
             </motion.div>
-            <motion.div 
+            <motion.div key={0}
             initial={{ opacity: 0, y: -100 }}
+            // transition={{ delay: 0.25 }}
             animate={{ opacity: 1, y: 0 }}
-               exit={{ opacity: 0, y: -100 }}>
+               exit={{ opacity: 0, y: -100 }}
+            className={classNames("min-h-screenHeightWithoutHeader", className)}>
                 {children}
             </motion.div>
         </div>
-        
     );
 }
