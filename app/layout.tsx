5d624c7cfd4a89342d42d3b4c5e0f6bae1872b7d
alexanderjalexander <alexj18082002@gmail.com>
2023-12-26 17:35:43 -0500

    Background scrolling issue fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,11 +30,12 @@ export default function RootLayout({children,}: {children: React.ReactNode;}) {
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head />
-			<body className={'min-h-screen bg-gradient-to-tr from-primary-900 via-background to-primary-900 '+fontSans.variable+' font-sans antialiased'}>
+			<body className={'min-h-screen '+fontSans.variable+' font-sans antialiased'}>
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<div className="relative flex flex-col h-screen">
 						<Navbar />
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow
+						bg-fixed bg-gradient-to-tr from-primary-900 via-background to-primary-900">
 							{children}
 						</main>
 						<footer className="w-full flex items-center justify-center py-3">
