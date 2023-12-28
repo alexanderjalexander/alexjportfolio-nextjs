704747054e22c16f069261496fd523a50b36b375
alexanderjalexander <alexj18082002@gmail.com>
2023-12-28 03:27:28 -0500

    Fixed fatal not-found error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import { Button } from '@nextui-org/button'
 export default function notFound() {
     return (
         <div>
-            <Header1Mono text="404" />
+            <Header1Mono>404</Header1Mono>
             <p className="text-center">We couldn&apos;t find the page you requested.</p>
             
         </div>
