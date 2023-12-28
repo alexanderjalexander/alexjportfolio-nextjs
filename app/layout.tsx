b8acc9228f50b20363ad5b837a2c0204a42c4fed
alexanderjalexander <alexj18082002@gmail.com>
2023-12-28 18:56:00 -0500

    background changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,12 +30,12 @@ export default function RootLayout({children,}: {children: React.ReactNode;}) {
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head />
-			<body className={'min-h-screen '+fontSans.variable+' font-sans antialiased'}>
+			<body className={'min-h-screen '+fontSans.variable+' font-sans antialiased' + 
+			' bg-fixed bg-gradient-to-tr from-primary-900 via-background to-primary-900'}>
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-					<div className="relative flex flex-col h-screen">
+					<div>
 						<Navbar />
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow
-						bg-fixed bg-gradient-to-tr from-primary-900 via-background to-primary-900">
+						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
 							{children}
 						</main>
 						<footer className="w-full flex items-center justify-center py-3">
