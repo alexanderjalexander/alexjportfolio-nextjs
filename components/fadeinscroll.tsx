fa76c02d124452c5e14cd6ee19cccdb500b64849
alexanderjalexander <alexj18082002@gmail.com>
2024-01-28 03:05:20 -0500

    console logs commented

--- components/fadeinscroll.tsx
+++ components/fadeinscroll.tsx
@@ -12,9 +12,9 @@ export function FadeInScroll({children, className,}:
             viewport={{ once: true, amount: "some", margin: "-20%" }}
             transition={{ duration: 1, type: 'spring' }}
             className={classNames("min-h-screenHeightWithoutHeader", className)}
-            onViewportEnter={() => (console.log('Object entered viewport.'))}
-            onAnimationStart={() => (console.log('FadeInScroll Animation Started...'))}
-            onAnimationComplete={() => (console.log('FadeInScroll Animation Complete!'))}
+            // onViewportEnter={() => (console.log('Object entered viewport.'))}
+            // onAnimationStart={() => (console.log('FadeInScroll Animation Started...'))}
+            // onAnimationComplete={() => (console.log('FadeInScroll Animation Complete!'))}
         >
             {children}
         </motion.div>
