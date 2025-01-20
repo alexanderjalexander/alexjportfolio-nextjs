caa816399252aa18fa5bf934814fccd43910d0ed
Alexander J <129627021+alexanderjalexander@users.noreply.github.com>
2025-01-20 17:50:53 -0500

    Database caching

--- src/lib/admin/categories-and-skills.ts
+++ src/lib/admin/categories-and-skills.ts
@@ -1,44 +1,62 @@
 import { getDatabase } from "@/src/db";
 import { skills, categories, skillCategory } from "@/src/db/migrations/schema";
 import {getTableColumns} from "drizzle-orm";
+import {unstable_cache} from "next/cache";
 
 export type tableDataType = {
     columns: { column: string, label: string }[],
     data: {}[]
 }
 
-export async function getAllSkills(): Promise<tableDataType> {
-    const data = await (await getDatabase())
-        .select()
-        .from(skills)
-        .execute();
-    return {
-        columns: Object.keys(getTableColumns(skills))
-            .map((col) => ({column: col, label: col.toUpperCase()})),
-        data: data,
-    }
-}
+const options = (tags?:string[]) => ({
+    tags: tags,
+    revalidate: 86400,
+})
 
-export async function getAllCategories(): Promise<tableDataType> {
-    const data = await (await getDatabase())
-        .select()
-        .from(categories)
-        .execute();
-    return {
-        columns: Object.keys(getTableColumns(categories))
-            .map((col) => ({column: col, label: col.toUpperCase()})),
-        data: data,
-    }
-}
+export const getAllSkills = unstable_cache(
+    async () => {
+        const data = await (await getDatabase())
+            .select()
+            .from(skills)
+            .execute();
+        return {
+            columns: Object.keys(getTableColumns(skills))
+                .map((col) => ({column: col, label: col.toUpperCase()})),
+            data: data,
+        }
+    },
+    ['skills'],
+    options()
+);
 
-export async function getAllSkillCategoryMappings(): Promise<tableDataType> {
-    const data = await (await getDatabase())
-        .select()
-        .from(skillCategory)
-        .execute();
-    return {
-        columns: Object.keys(getTableColumns(skillCategory))
-            .map((col) => ({column: col, label: col.toUpperCase()})),
-        data: data,
-    }
-}
+export const getAllCategories = unstable_cache(
+    async () => {
+        const data = await (await getDatabase())
+            .select()
+            .from(categories)
+            .execute();
+        return {
+            columns: Object.keys(getTableColumns(categories))
+                .map((col) => ({column: col, label: col.toUpperCase()})),
+            data: data,
+        }
+    },
+    ['categories'],
+    options()
+);
+
+export const getAllSkillCategoryMappings = unstable_cache(
+    async () => {
+        const data = await (await getDatabase())
+            .select()
+            .from(skillCategory)
+            .execute();
+        return {
+            columns: Object.keys(getTableColumns(skillCategory))
+                .map((col) => ({column: col, label: col.toUpperCase()})),
+            data: data,
+        }
+    },
+    ['skillCategory'],
+    options()
+);
