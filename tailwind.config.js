2548f695220ad0998f761d454b75f71ea6df9864
Alexander J <129627021+alexanderjalexander@users.noreply.github.com>
2026-03-12 22:40:29 -0400

    Vulnerability update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,22 @@ import { heroui } from "@heroui/theme";
 const safelist = [
   "m-1",
   "dark:bg-red-700 bg-red-300",
-  "dark:bg-cyan-700 bg-cyan-300",
-  "dark:bg-purple-700 bg-purple-300",
+  "dark:bg-fuchsia-700 bg-fuchsia-300",
   "dark:bg-yellow-700 bg-yellow-300",
+  "dark:bg-green-700 bg-green-300",
+  "dark:bg-cyan-700 bg-cyan-300",
+  "dark:bg-blue-700 bg-blue-300",
+  "dark:bg-pink-700 bg-pink-300",
+  "dark:bg-slate-700 bg-slate-300",
+  // extensions
+  "dark:bg-orange-900 bg-orange-400",
+  "dark:bg-violet-700 bg-violet-300",
+  "dark:bg-teal-700 bg-teal-300",
+  "dark:bg-lime-700 bg-lime-300",
+  "dark:bg-indigo-700 bg-indigo-300",
+  "dark:bg-rose-700 bg-rose-300",
+  "dark:bg-amber-700 bg-amber-300",
   "dark:bg-emerald-700 bg-emerald-300",
-  "dark:bg-orange-700 bg-orange-300",
 ];
 
 /** @type {import('tailwindcss').Config} */
