db2d230d716ba336513268cdc76f33fc95543597
Alexander J <129627021+alexanderjalexander@users.noreply.github.com>
2026-06-29 21:07:06 -0400

    Removal of pg Pool

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,17 +2,17 @@ import { neon } from "@neondatabase/serverless";
 import type { NeonQueryFunction } from "@neondatabase/serverless";
 import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
 import { drizzle as drizzlePg } from 'drizzle-orm/node-postgres';
-import { Pool } from "pg";
 
 // Consider using driver over websockets for interactive transactions
 // https://neon.tech/docs/serverless/serverless-driver#use-the-driver-over-websockets
 export async function getDatabase() {
   if (process.env.HOMELAB_DATABASE_URL) {
-    const pool = new Pool({
-      connectionString: process.env.HOMELAB_DATABASE_URL,
-      ssl: false,
-    })
-    return drizzlePg(pool);
+    return drizzlePg({
+      connection: {
+        connectionString: process.env.HOMELAB_DATABASE_URL,
+        ssl: false,
+      }
+    });
   }
 
   if (process.env.NEON_DATABASE_URL) {
