1d50821c324e73da27b5adde20c5022d1e3cef53
Alexander J <129627021+alexanderjalexander@users.noreply.github.com>
2026-07-03 17:11:16 -0400

    Webring Styling & Behavior Updates

--- components/webring.tsx
+++ components/webring.tsx
@@ -1,4 +1,5 @@
-// import { Link } from "@heroui/link";
+import { LINK_STYLE } from "@/config/constants";
+import { bricolageGrotesque } from "@/fonts/config";
 
 interface WebringPerson {
   id: string;
@@ -25,25 +26,43 @@ export default async function Webring() {
     });
     random = await response2.json();
     return (
-      <div className="m-auto w-fit pb-6 text-xs text-center">
+      <div
+        className={`m-auto w-fit pb-6 text-md text-center`}
+      >
         <div>
           &lt;-{' '}
-          <a className="text-xs" href={neighbors[0].url}>
+          <a
+            className={`${bricolageGrotesque.className} ${LINK_STYLE} font-extrabold`}
+            href={neighbors[0].url}
+            target="_blank"
+          >
             {neighbors[0].name}
           </a>
-          &bull;{' '}
-          <a className="text-xs" href={random.url}>
+          {' '}&bull;{' '}
+          <a
+            className={`${bricolageGrotesque.className} ${LINK_STYLE} font-extrabold`}
+            href={random.url}
+            target="_blank"
+          >
             Random
           </a>
-          &bull;{' '}
-          <a className="text-xs" href={neighbors[1].url}>
+          {' '}&bull;{' '}
+          <a
+            className={`${bricolageGrotesque.className} ${LINK_STYLE} font-extrabold`}
+            href={neighbors[1].url}
+            target="_blank"
+          >
             {neighbors[1].name}
           </a>
-          -&gt;
+          {' '}-&gt;
         </div>
         <div>
           This website is part of the{' '}
-          <a className="text-xs" href={'https://github.com/Stevens-26/webring'}>
+          <a
+            className={`${bricolageGrotesque.className} ${LINK_STYLE} font-extrabold`}
+            href={'https://github.com/Stevens-26/webring'}
+            target="_blank"
+          >
             Stevens Community Webring.
           </a>
         </div>
